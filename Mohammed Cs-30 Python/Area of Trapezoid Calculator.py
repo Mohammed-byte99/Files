b5c31f796529f0b2855c1853bb15b865662eddf5
Mohammed-byte99 <70777216+Mohammed-byte99@users.noreply.github.com>
2020-09-08 13:37:28 -0600

    Add files via upload

---
+++ Mohammed Cs-30 Python/Area of Trapezoid Calculator.py
@@ -0,0 +1,16 @@
+print("Area of a trapezoid")
+ 
+height = input("Enter the height of the trapezoid:")
+height = float(height)
+ 
+length_bottom = input("Enter the length of the bottom base:")
+length_bottom = float(length_bottom)
+ 
+length_top = input("Enter the length of the top base:")
+length_top = float(length_top)
+ 
+area = 0.5 * (length_bottom + length_top) * height
+print("The area is:", area)
+
+
+
